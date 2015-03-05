5ffb33b01af6c4241a5610e881af6392b8844e50
Tobias Schürg <tobiasschuerg@gmail.com>
2015-03-05 12:58:05 +0100

    renamed main class

--- src/com/tobiasschuerg/spheremenu/SphereMenuExample.java
+++ src/com/tobiasschuerg/spheremenu/SphereMenuExample.java
@@ -1,7 +1,6 @@
 package com.tobiasschuerg.spheremenu;
 
 import com.jme3.app.SimpleApplication;
-import com.jme3.light.AmbientLight;
 import com.jme3.light.DirectionalLight;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
@@ -10,48 +9,29 @@ import com.jme3.math.Quaternion;
 import com.jme3.math.Vector3f;
 import com.jme3.renderer.RenderManager;
 import com.jme3.scene.Geometry;
-import com.jme3.scene.Node;
-import com.jme3.scene.shape.Box;
 import com.jme3.scene.shape.Quad;
-import com.jme3.texture.Texture;
-import java.util.ArrayList;
-import java.util.Random;
 
 /**
- * test
+ * Example of the Sphere Menu
  *
  * @author normenhansen
  */
-public class Main extends SimpleApplication {
+public class SphereMenuExample extends SimpleApplication {
 
     public static void main(String[] args) {
-        Main app = new Main();
+        SphereMenuExample app = new SphereMenuExample();
         app.start();
     }
 
     @Override
     public void simpleInitApp() {
         createFloor();
-
-        Box b = new Box(1, 1, 1);
-        Geometry geom = new Geometry("Box", b);
-
-        Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        mat.setColor("Color", ColorRGBA.Blue);
-        geom.setMaterial(mat);
-
-        // rootNode.attachChild(geom);
+        createSphereMenu();
 
         DirectionalLight sun = new DirectionalLight();
         sun.setColor(ColorRGBA.Red);
         sun.setDirection(new Vector3f(-.5f, -.5f, -.5f).normalizeLocal());
         rootNode.addLight(sun);
-
-        AmbientLight al = new AmbientLight();
-        al.setColor(ColorRGBA.White.mult(1.3f));
-        rootNode.addLight(al);
-
-        createSphereMenu();
     }
 
     @Override
@@ -66,27 +46,28 @@ public class Main extends SimpleApplication {
 
     private void createFloor() {
         Quad q = new Quad(20f, 20f);
-        Geometry geo = new Geometry("floor", q);
+        Geometry floor = new Geometry("floor", q);
         Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
         mat.setColor("Color", ColorRGBA.Brown);
-        geo.setMaterial(mat);
-
-
-
-        geo.rotate(PITCH270);
-        geo.move(getCamera().getLocation().add(new Vector3f(-10f, -1f, 10f)));
-
-        rootNode.attachChild(geo);
+        floor.setMaterial(mat);
+        floor.rotate(PITCH270);
+        floor.move(getCamera().getLocation().add(new Vector3f(-15f, -1f, 15f)));
+        rootNode.attachChild(floor);
     }
 
     private void createSphereMenu() {
+        // create the menu
         SphereMenu menu = new SphereMenu(this);
+
+        // set up the menu
         menu.setSize(45, 75);
         menu.setRadius(2f);
         menu.setColumns(2);
 
+        // create / update the menu
         menu.create(4);
 
+        // attach it and move it around user
         rootNode.attachChild(menu);
         menu.move(getCamera().getLocation());
         menu.rotate(getCamera().getRotation());
